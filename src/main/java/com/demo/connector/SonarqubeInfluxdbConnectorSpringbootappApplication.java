70e17c556c242dc47b18cfc230a0d0e2084f0501
Vinay Chaudhary <yq17@clarica.com>
2021-09-15 00:16:07 +0530

    Project initial structure added

---
+++ src/main/java/com/demo/connector/SonarqubeInfluxdbConnectorSpringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.demo.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeInfluxdbConnectorSpringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeInfluxdbConnectorSpringbootappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/connector/SonarqubeInfluxdbConnectorSpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeInfluxdbConnectorSpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
