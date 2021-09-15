ad3bacb18fe05f01180a7aae98e09aaf88758476
Vinay Chaudhary <yq17@clarica.com>
2021-09-15 12:59:36 +0530

    file added

---
+++ src/main/java/com/demo/connector/utility/SonarConstant.java
@@ -0,0 +1,24 @@
+package com.demo.connector.utility;
+
+public class SonarConstant {
+
+	public static final String SonarAPIQueryParam_ComponentKeys = "componentKeys";
+	public static final String SonarAPIQueryParam_Branch = "branch";
+	public static final String SonarAPIQueryParam_AdditionalFields = "additionalFields";
+	public static final String SonarAPIQueryParam_Types = "types";
+	public static final String SonarAPIQueryParam_Statuses = "statuses";
+	public static final String SonarAPIResponse_Project="project";
+	public static final String SonarAPIResponse_Application="application";
+	public static final String SonarAPIResponse_Branch="branch";
+	public static final String SonarAPIResponse_Component="component";
+	public static final String SonarAPIResponse_Line="line";
+	public static final String SonarAPIResponse_Severity="severity";
+	public static final String SonarAPIResponse_UpdateDate="updateDate";
+	public static final String SonarAPIResponse_Author="author";
+	public static final String SonarAPIResponse_Message="message";
+
+	public static final String SonarMeasurementAPIParam_MetricKeys="metricKeys";
+	public static final String SonarMeasurementAPIParam_Component="component";
+	public static final String SonarMeasurementAPI_Measurement="MEASURES";
+
+}
